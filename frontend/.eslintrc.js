0394f81d6644d3d1370f2a1ec51b13f4d09c8e6a
Nacho Pinero <piwero@gmail.com>
2022-11-06 19:32:16 +0000

    Add rules to eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -19,6 +19,10 @@ module.exports = {
         "react",
         "@typescript-eslint"
     ],
-    "rules": {
-    }
+     rules: {
+    // suppress errors for missing 'import React' in files
+   "react/react-in-jsx-scope": "off",
+    // allow jsx syntax in js files (for next.js project)
+   "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts"] }], //should add ".ts" if typescript project
+  }
 }
